91863fe3b7f64d4b01c405652b8bf178596db81d
dkimperia <kogutdani1@yandex.ru>
2024-02-02 00:37:20 +0600

    just server

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,1 +1,15 @@
-print("Lets start")
+import jwt, datetime, os
+from flask import Flask, request
+# from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
+
+server = Flask(__name__)
+#object of Flask class
+pymysql.install_as_MySQLdb()
+mysql = MySQL(server)
+
+#config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+print(server.config["MYSQL_HOST"])
