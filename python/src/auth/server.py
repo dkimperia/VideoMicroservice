b7a75f073eb3c33893cb60be40d5d773c357e25b
dkimperia <kogutdani1@yandex.ru>
2024-02-05 18:23:14 +0600

    validate functions

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -41,16 +41,47 @@ def login():
 		if auth.username != email or auth.password != password:
 			return "Invalid credentials", 401
 		else:
-			return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+			return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)#check JWT
 
 	else:
 		return "Invalid credentials", 401
 
 
+@server.route("/validate", method=["POST"])
+def validate():
+	encoded_jwt = request.headers["authorization"]
+
+	if not encoded_jwt:
+		return "missing credentials", 401
+
+	encoded_jwt = encoded_jwt.split(" ")[1]
+	try:
+		decode = jwt.decode(
+			encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=['HS256']
+		)
+	except:
+		return 'not authorized', 403 
+
+	return decoded, 200
 
 
 
+#True or false admin or net
+def createJWT(username, secret, authz):
+	return jwt.encode(
+		{
+			"username": username,
+			"exp": datetime.datetime.now(tz=datetime.timezone.utc)
+			+ datetime.timedelta(days = 1),
+			"iat": datetime.datetime.utcnow()
+			"admin": authz,
+		},
+		secret,
+		algorithm = "HS256"
+	)
 
+if __name__ == "__main__":
+	server.run(host = "0.0.0.0", port = 5000)
 
 
 
